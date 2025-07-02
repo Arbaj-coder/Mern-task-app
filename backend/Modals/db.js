0431501fc32db2c20c21d05ec97a680e0de166bb
Arbaj-coder <2024kucp1060@iiitkota.ac.in>
2025-07-02 10:38:11 +0530

    Update db.js

--- backend/Modals/db.js
+++ backend/Modals/db.js
@@ -1,24 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose  = require('mongoose')
 
-let isConnected;
+const mongo_url = process.env.MONGO_CONN;
 
-const connectDB = async () => {
-  if (isConnected) {
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = db.connections[0].readyState;
-    console.log('✅ MongoDB Connected');
-  } catch (error) {
-    console.error('❌ MongoDB connection error:', error.message);
-    throw error;
-  }
-};
-
-module.exports = connectDB;
+mongoose.connect(mongo_url)
+    .then(()=>{
+        console.log("mongoDB connected...")
+    }).catch((err)=>{
+        console.log("mongodb connection error " , err);
+    })
