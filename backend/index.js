5e68da83d36fd53a7cf372cfb2a73bfa09501a65
Arbaj-coder <2024kucp1060@iiitkota.ac.in>
2025-07-02 10:39:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
-const TaskRouter = require('./Routes/TaskRouter');
-const connectDB = require('./db');
+const TaskRouter = require('./Routes/TaskRouter')
 
-connectDB();
 require('dotenv').config();
 require('./Modals/db');
 const PORT = process.env.PORT || 8080;
